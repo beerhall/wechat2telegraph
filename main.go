4774be7ef4ff2b1e6e9e23fb6a22cec0dc7c8f95
beerhall <wzh970719@gmail.com>
2019-02-05 21:29:59 +0800

    Add input auth

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"image"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 
@@ -60,65 +61,69 @@ func main() {
 	go http.ListenAndServeTLS("0.0.0.0:"+port, certFile, keyFile, nil)
 
 	for update := range updates {
-		// Request the HTML page.
-		res, err := http.Get(update.Message.Text)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer res.Body.Close()
-		if res.StatusCode != 200 {
-			log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-		}
-
-		// Load the HTML document
-		doc, err := goquery.NewDocumentFromReader(res.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		doc.Find("img").Each(func(i int, s *goquery.Selection) {
-			data_src, _ := s.Attr("data-src")
-			filename := fmt.Sprintf("%x", sha512.Sum512([]byte(data_src)))[0:12]
-
-			cmd := exec.Command("curl", "-o", websiteFolder+imageFolder+filename, data_src)
-			log.Info("Running command and waiting for it to finish...")
-			cmd.Run()
-
-			f, _ := os.Open(websiteFolder + imageFolder + filename)
-			r := bufio.NewReader(f)
-			_, format, _ := image.DecodeConfig(r)
-
-			os.Rename(websiteFolder+imageFolder+filename, websiteFolder+imageFolder+filename+"."+format)
-			s.SetAttr("src", "https://"+address+"/"+imageFolder+filename+"."+format)
-			log.Info("filename:\t" + filename + "." + format)
-		})
-
-		title, _ := doc.Find("#activity-name").First().Html()
-		author, _ := doc.Find("#js_name").First().Html()
-		html, _ := doc.Find("#js_content").First().Html()
-
-		if client, err := telegraph.Create(author, author, ""); err == nil {
-			log.Info("> Created client: %#+v", client)
-
-			// GetAccountInfo
-			if account, err := client.GetAccountInfo(nil); err == nil {
-				log.Info("> GetAccountInfo result: %#+v", account)
-			} else {
-				log.Info("* GetAccountInfo error: %s", err)
+		if u, err := url.Parse(update.Message.Text); (err == nil) && (u.Hostname() == "mp.weixin.qq.com") {
+			// Request the HTML page.
+			res, err := http.Get(update.Message.Text)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer res.Body.Close()
+			if res.StatusCode != 200 {
+				log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 			}
 
-			// CreatePage
-			if page, err := client.CreatePageWithHTML(title, author, "", html, false); err == nil {
-				log.Info("> CreatePage result: %#+v", page)
-				log.Info("> Created page url: %s", page.URL)
-
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, page.URL)
-
-				bot.Send(msg)
-
-			} else {
-				log.Error("* CreatePage error: %s", err)
+			// Load the HTML document
+			doc, err := goquery.NewDocumentFromReader(res.Body)
+			if err != nil {
+				log.Fatal(err)
 			}
+
+			doc.Find("img").Each(func(i int, s *goquery.Selection) {
+				data_src, _ := s.Attr("data-src")
+				filename := fmt.Sprintf("%x", sha512.Sum512([]byte(data_src)))[0:12]
+
+				cmd := exec.Command("curl", "-o", websiteFolder+imageFolder+filename, data_src)
+				log.Info("Running command and waiting for it to finish...")
+				cmd.Run()
+
+				f, _ := os.Open(websiteFolder + imageFolder + filename)
+				r := bufio.NewReader(f)
+				_, format, _ := image.DecodeConfig(r)
+
+				os.Rename(websiteFolder+imageFolder+filename, websiteFolder+imageFolder+filename+"."+format)
+				s.SetAttr("src", "https://"+address+"/"+imageFolder+filename+"."+format)
+				log.Info("filename:\t" + filename + "." + format)
+			})
+
+			title, _ := doc.Find("#activity-name").First().Html()
+			author, _ := doc.Find("#js_name").First().Html()
+			html, _ := doc.Find("#js_content").First().Html()
+
+			if client, err := telegraph.Create(author, author, ""); err == nil {
+				log.Info("> Created client: %#+v", client)
+
+				// GetAccountInfo
+				if account, err := client.GetAccountInfo(nil); err == nil {
+					log.Info("> GetAccountInfo result: %#+v", account)
+				} else {
+					log.Info("* GetAccountInfo error: %s", err)
+				}
+
+				// CreatePage
+				if page, err := client.CreatePageWithHTML(title, author, "", html, false); err == nil {
+					log.Info("> CreatePage result: %#+v", page)
+					log.Info("> Created page url: %s", page.URL)
+
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, page.URL)
+
+					bot.Send(msg)
+
+				} else {
+					log.Error("* CreatePage error: %s", err)
+				}
+			}
+		} else {
+			log.Errorf("Not URL or Host name error: %s", err)
 		}
 	}
 }
